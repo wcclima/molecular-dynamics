663957900b253ce3491f735c6dfc0ca52bf500f0
William Lima <107582520+wcclima@users.noreply.github.com>
2024-07-13 12:00:58 +0200

    Update _thermostat_func.py

--- molecular_dynamics/_thermostat_func.py
+++ molecular_dynamics/_thermostat_func.py
@@ -13,16 +13,32 @@ def thermostat(
         dist_func: Callable[[float, float], Tuple[float, float, float]], 
         boundary_cond_func: Callable[[np.ndarray, np.ndarray], Tuple[np.ndarray, np.ndarray]], 
         wall_force_func: Callable[[np.ndarray, np.ndarray], Tuple[np.ndarray, np.ndarray]]
-        ):
+        ) -> None:
+    
+    """
+    Implements a thermostat using the velocity rescaling method. 
+    It rescale the molecules's velocity, evolve the gas starting 
+    from this new initial velocity and then concatenates the gas 
+    state history at the new temperature to the old one.
 
 
-    thermostat_factor = new_temperature/self.average_kT
+    Keyword arguments:
+    
+    new_temperature (float) -- the new temperature the gas should achieve
+    extra_time_steps (int) -- the number of steps to the gas approach the new thermal equilibrium
+    dist_func (callable) -- the function defining the pairwise distance between the molecules
+    boundary_cond_func (callable) -- the function defining the boundary condition
+    wall_force_func (callable) -- the function defining a har wall, if any  
+    """
+
+
+    vel_rescaling_factor = np.sqrt(new_temperature/self.average_kT)
 
     x_coord_prev, y_coord_prev = self.data[self.total_time_steps - 2][0:2]
     x_coord, y_coord, vx, vy= self.data[self.total_time_steps - 1]
 
-    vx[self.gas_molecules_index] = thermostat_factor*vx[self.gas_molecules_index]
-    vy[self.gas_molecules_index] = thermostat_factor*vy[self.gas_molecules_index]
+    vx[self.gas_molecules_index] = vel_rescaling_factor*vx[self.gas_molecules_index]
+    vy[self.gas_molecules_index] = vel_rescaling_factor*vy[self.gas_molecules_index]
 
     self.data[self.total_time_steps - 1][2] = vx
     self.data[self.total_time_steps - 1][3] = vy
@@ -30,8 +46,8 @@ def thermostat(
     x_coord_prev[self.gas_molecules_index], y_coord_prev[self.gas_molecules_index] = (
         boundary_cond_func(
             self, 
-            x_coord[self.gas_molecules_index] - (x_coord[self.gas_molecules_index] - x_coord_prev[self.gas_molecules_index])*thermostat_factor, 
-            y_coord[self.gas_molecules_index] - (y_coord[self.gas_molecules_index] - y_coord_prev[self.gas_molecules_index])*thermostat_factor
+            x_coord[self.gas_molecules_index] - (x_coord[self.gas_molecules_index] - x_coord_prev[self.gas_molecules_index])*vel_rescaling_factor, 
+            y_coord[self.gas_molecules_index] - (y_coord[self.gas_molecules_index] - y_coord_prev[self.gas_molecules_index])*vel_rescaling_factor
             )
         )
 
